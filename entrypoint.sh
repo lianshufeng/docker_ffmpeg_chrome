3247cc02e86f1421843af79b989ae099cb994181
lianshufeng <251708339@qq.com>
2020-03-25 16:57:41 +0800

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,6 @@ if [ -z  $FFMPEG_CMD ]; then
 	 -f x11grab -video_size ${SCREEN_WIDTH}x${SCREEN_HEIGHT} \
 	 -i $DISPLAY -async 1 -vsync 1 \
 	 -framerate ${FRAMERATE} -vcodec libx264 -preset ultrafast -qp 0 -pix_fmt yuv444p \
-	 -vf \"drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf: text='\\%{localtime\:\\%m/\\%d/\\%Y \\%I.\\%M.\\%S \\%p}': fontcolor=white@0.8: x=10: y=950\" \
 	 -tune zerolatency -b 900k -f flv $STREAM_URL"
 fi
 
