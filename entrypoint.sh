68d22ce2960c95229698c607966ddae34a0509ec
lianshufeng <251708339@qq.com>
2020-03-25 14:59:30 +0800

    修改命令行

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,7 @@ ffmpeg -f x11grab -video_size ${SCREEN_WIDTH}x${SCREEN_HEIGHT} \
  -framerate ${framerate} -vcodec libx264 -preset ultrafast -qp 0 -pix_fmt yuv444p \
  -vf "drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf: text='%{localtime\:%m/%d/%Y %I.%M.%S %p}': fontcolor=white@0.8: x=10: y=950" \
  -tune zerolatency -b 900k \
- -f pulse -i default \
- -f flv $STREAM_URL
+ -f pulse -i default flv $STREAM_URL
 
 
 # SAMPLE: rtmp://192.168.1.66:1935/live/abc
